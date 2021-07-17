064293330cef1e68195c14887a161b21b6165b5d
fmabid <fmaabid@gmail.com>
2021-07-17 14:14:17 +0600

    project init

---
+++ components/Tutorial.vue
@@ -0,0 +1,6 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top min-h-screen bg-gray-100 sm:pt-0">
+    <div class="w-full bg-gray-600">init</div>
+  </div>
+</template>
