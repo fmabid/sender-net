1e44458d2484c3d07a99c5f6a76ba8f8a15bca01
fmabid <fmaabid@gmail.com>
2021-07-18 22:00:13 +0600

    assets as component

---
+++ components/utility/Tag.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span class="py-0.5 px-2 rounded-full bg-gray-300 text-xs text-gray-600">
+      {{ tagName }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tagName: {
+      default: '',
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
